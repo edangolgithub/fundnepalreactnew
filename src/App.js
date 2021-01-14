9003efafea34259e1f63e8148954bb0bf885eb9e
edangolgithub <dangolevn@gmail.com>
2021-01-14 04:04:10 +0000

    iuuy

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
-import {  withAuthenticator } from '@aws-amplify/ui-react';
+import { withAuthenticator } from '@aws-amplify/ui-react';
 
 import './App.css';
-import  Eauth  from "./Authentication/Eauth";
 
+import Navfundnepal from './components/Navfundnepal'
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Navfundnepal />
         <h1>Fund Nepal 2021</h1>
-       <Eauth className="a" />
+
         <h2>My App Content</h2>
       </header>
+
+
+
     </div>
   );
 }
